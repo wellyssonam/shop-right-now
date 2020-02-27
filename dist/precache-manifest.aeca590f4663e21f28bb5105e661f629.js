e817d69a5a53d098a043f70215d4f61b2dc6fa19
Wellysson <Wellyssonam@gmail.com>
2020-02-26 23:23:08 -0300

    feat: build-dist
    Generate dist directory

---
+++ dist/precache-manifest.aeca590f4663e21f28bb5105e661f629.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb887800bfb6ede3883",
+    "url": "/css/app.44b7c64e.css"
+  },
+  {
+    "revision": "5326184bfdb072cb52be",
+    "url": "/css/chunk-vendors.eb96d498.css"
+  },
+  {
+    "revision": "c35a96d824cd556a05e87b53907cc4b1",
+    "url": "/img/flag-brazil.c35a96d8.png"
+  },
+  {
+    "revision": "d0972b2b9d4904f2c25959082b658a54",
+    "url": "/img/flag-usa.d0972b2b.png"
+  },
+  {
+    "revision": "3bede0b5898b0455e97ac72a55b1a53e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cb887800bfb6ede3883",
+    "url": "/js/app.dcfb79d1.js"
+  },
+  {
+    "revision": "250780d50126c4b3126b",
+    "url": "/js/chunk-2d0d0a08.ac7da4b3.js"
+  },
+  {
+    "revision": "5326184bfdb072cb52be",
+    "url": "/js/chunk-vendors.50c27d66.js"
+  },
+  {
+    "revision": "9a5ed2ba3d16fdf3deac85e1b846ee8f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
